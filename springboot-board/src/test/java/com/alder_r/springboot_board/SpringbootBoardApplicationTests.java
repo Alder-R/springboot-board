fe404aa45c26dd4c422ca83d75f9fa4d1220da7e
Alder-R <kdh012356@gmail.com>
2025-06-11 13:43:32 +0900

    프로젝트 생성

---
+++ springboot-board/src/test/java/com/alder_r/springboot_board/SpringbootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alder_r.springboot_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
